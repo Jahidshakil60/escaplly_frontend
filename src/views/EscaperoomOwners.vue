153c0308919a01846cdc0a901ff97822ae78b9de
Shakil <shakil.dgency@gmail.com>
2022-12-12 17:25:56 +0600

    design of escaperoom owner page

--- src/views/EscaperoomOwners.vue
+++ src/views/EscaperoomOwners.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="escaperoom-owner">
-    <BreadCrumb />
-    <section class="escaperoom-owner__headsection g-section--structure">
-      <div class="headsection-main body-container">
+	<div class="escaperoom-owner">
+		<BreadCrumb />
+		<section class="escaperoom-owner__headsection g-section--structure">
+			<div class="headsection-main body-container">
 				<div class="headsection-main__text">
 					<h2>Unlock The Potential Of Your Escape Room Business Growth</h2>
 					<p>Lorem Ipsum is simply dummy text of the text a Lorem Ipsum is simply dummy text.</p>
@@ -14,10 +14,168 @@
 					<img src="@/assets/img/escaperoom__owners.jpg" alt="" />
 				</div>
 			</div>
-    </section>
-  </div>
-</template>
+		</section>
+		<section class="escaperoom-owner__get-from-us g-section--structure">
+			<div class="get-from-us-main body-container">
+				<div class="get-from-us-main__text">
+					<h2>You Will Get From Us!</h2>
+					<p>Lorem Ipsum is simply dummy text of the text a Lorem Ipsum is simply dummy text of the text a lpsum simply text for dummy only.</p>
+				</div>
+				<div class="get-from-us-main__flex-item">
+					<div class="flex-item">
+						<h5><span>1</span> More Booking</h5>
+						<p>First, you'll pick which epic, themed escape room you want to play! Themes range.</p>
+					</div>
+					<div class="flex-item">
+						<h5><span>2</span> New Customers</h5>
+						<p>First, you'll pick which epic, themed escape room you want to play! Themes range.</p>
+					</div>
+					<div class="flex-item">
+						<h5><span>3</span> Zero Risk</h5>
+						<p>First, you'll pick which epic, themed escape room you want to play! Themes range.</p>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="escaperoom-owner__responsibility g-section--structure">
+			<div class="g--support-system__main body-container">
+				<div class="g--support-system-text">
+					<h3>Our Team Is Ready To Help You, It's Our Responsibility To Grow Your Business.</h3>
+					<p>
+						Lorem Ipsum is simply dummy text of the text a Lorem Ipsum is simply dummy text of the text a lpsum simply text for dummy only. simply
+						dummy text of the text a lpsum simply text for dummy only.
+					</p>
+					<div class="btn">
+						<button class="g--page-button">Contact Us</button>
+					</div>
+				</div>
+				<div class="g--support-system-img">
+					<img src="@/assets/img/support_system.jpg" alt="" />
+				</div>
+			</div>
+		</section>
+		<section class="escaperoom-owner__escaplly-best g-section--structure">
+			<div class="escaplly-best-main body-container">
+				<div class="escaplly-best-wrapper">
+					<div class="escaplly-best-wrapper__text">
+						<h3>Value Proposition From Escaplly, Committed To You!</h3>
+						<p>
+							Lorem Ipsum is simply dummy text of the text a Lorem Ipsum is simply dummy text of the text a lpsum simply text for dummy only. simply
+							dummy text of the text a lpsum simply text for dummy only. Lorem Ipsum is simply dummy text of the text a Lorem Ipsum is simply dummy
+							text of the text a lpsum simply text for dummy only.
+						</p>
+						<div class="btn">
+							<button class="g--page-button">Partner Request Form</button>
+						</div>
+					</div>
+					<div class="escaplly-best-wrapper__items">
+						<div class="item-popular item">
+							<h6>Expand Your Reach</h6>
+							<p>To move forward and open certain doors or chests, you will need to find physical keys or digital.</p>
+						</div>
+						<div class="item-booking item">
+							<h6>Only Pay For Result</h6>
+							<p>To move forward and open certain doors or chests, you will need to find physical keys or digital.</p>
+						</div>
+						<div class="item-coupon item">
+							<h6>No Direct Competition</h6>
+							<p>To move forward and open certain doors or chests, you will need to find physical keys or digital.</p>
+						</div>
+						<div class="item-review item">
+							<h6>Most Advanced Sponsors Features</h6>
+							<p>To move forward and open certain doors or chests, you will need to find physical keys or digital.</p>
+						</div>
+					</div>
+				</div>
+				<div class="escaplly-main__details">
+					<h3>Our Clients, We Help To Grow Their Business</h3>
+					<div class="listed-company-logo">
+						<div class="container">
+							<img src="@/assets/img/alin.png" alt="" />
+						</div>
+						<div class="container">
+							<img src="@/assets/img/riddle.png" alt="" />
+						</div>
+						<div class="container">
+							<img src="@/assets/img/home_town.png" alt="" />
+						</div>
+						<div class="container">
+							<img src="@/assets/img/wayof.png" alt="" />
+						</div>
+						<div class="container">
+							<img src="@/assets/img/claverfox.png" alt="" />
+						</div>
+						<div class="container">
+							<img src="@/assets/img/inferno.png" alt="" />
+						</div>
+					</div>
+					<div class="btn">
+						<button class="g--page-button">View All Listed Companies</button>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="escaperoom-owner__become-partner g-section--structure">
+			<div class="become-partner__head">
+				<div class="head-section--style">
+					<h3>How To Become Partner With us!</h3>
+					<p>Lorem Ipsum is simply dummy text of the text a Lorem Ipsum is simply dummy text simply text for dummy only.</p>
+				</div>
+			</div>
 
+			<div class="become-partner-main body-container">
+				<div class="become-partner-main__step">
+					<div class="step-text">
+						<h3>STEP 1</h3>
+						<h5>Fill Out Partner Request From</h5>
+						<p>
+							Lorem Ipsum is simply dummy text of the text a Lorem Ipsum is simply dummy text of the text a lpsum simply text for dummy only. simply
+							dummy text of the text a lpsum simply text for dummy only..
+						</p>
+						<div class="btn">
+							<button class="g--page-button">Partner Request Form</button>
+						</div>
+					</div>
+					<div class="step-img">
+						<img src="@/assets/img/partner_form.png" alt="" />
+					</div>
+				</div>
+				<div class="become-partner-main__step step--2">
+					<div class="step-text">
+						<h3>STEP 2</h3>
+						<h5>15 Minute Onboarding Call</h5>
+						<p>
+							Lorem Ipsum is simply dummy text of the text a Lorem Ipsum is simply dummy text of the text a lpsum simply text for dummy only. simply
+							dummy text of the text a lpsum simply text for dummy only..
+						</p>
+						<div class="btn">
+							<button class="g--page-button">Partner Request Form</button>
+						</div>
+					</div>
+					<div class="step-img">
+						<img src="@/assets/img/twitter_activation.png" alt="" />
+					</div>
+				</div>
+				<div class="become-partner-main__step">
+					<div class="step-text">
+						<h3>STEP 1</h3>
+						<h5>Fill Out Partner Request From</h5>
+						<p>
+							Lorem Ipsum is simply dummy text of the text a Lorem Ipsum is simply dummy text of the text a lpsum simply text for dummy only. simply
+							dummy text of the text a lpsum simply text for dummy only..
+						</p>
+						<div class="btn">
+							<button class="g--page-button">Partner Request Form</button>
+						</div>
+					</div>
+					<div class="step-img">
+						<img src="@/assets/img/partner_form.png" alt="" />
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
+</template>
 
 <script>
 import BreadCrumb from "@/components/BreadCrumb.vue";
@@ -30,6 +188,4 @@ export default {
 };
 </script>
 
-<style lang="scss" src="@/assets/css/views/EscaperoomOwners.scss">
-
-</style>
+<style lang="scss" src="@/assets/css/views/EscaperoomOwners.scss"></style>
